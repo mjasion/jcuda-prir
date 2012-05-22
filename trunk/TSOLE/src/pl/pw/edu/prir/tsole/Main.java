718e9622d62ae751c756f61ed439a9f41bc0e29e
marcinjasion@gmail.com <marcinjasion@gmail.com@4c6f1809-d47f-6a53-e29b-218769a40873>
2012-05-22 18:22:17 +0000

    111

--- trunk/TSOLE/src/pl/pw/edu/prir/tsole/Main.java
+++ trunk/TSOLE/src/pl/pw/edu/prir/tsole/Main.java
@@ -3,11 +3,15 @@ package pl.pw.edu.prir.tsole;
 import jcuda.Pointer;
 import jcuda.runtime.JCuda;
 
+/**
+ * Parametry VM: 
+ * -Djava.library.path=${workspace_loc:TSOLE/JCuda-0.4.1/lib}
+ * @author Marcin
+ *
+ */
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Pointer pointer = new Pointer();
         JCuda.cudaMalloc(pointer, 4);
