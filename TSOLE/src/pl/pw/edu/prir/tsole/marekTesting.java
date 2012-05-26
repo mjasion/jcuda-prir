b6d17ba47ea2f031f65cbf7450c56f2be0244a0c
msarnecki <msarneck@gmail.com>
2012-05-26 21:29:46 +0000

    drobne komentarze

--- TSOLE/src/pl/pw/edu/prir/tsole/marekTesting.java
+++ TSOLE/src/pl/pw/edu/prir/tsole/marekTesting.java
@@ -5,6 +5,12 @@ import jcuda.Sizeof;
 import jcuda.jcublas.JCublas;
 
 
+/**
+ * 
+ * 
+ * SGEEM - to operacja mnozenia macierzy pojedynczej precyzji
+ *
+ */
 
 
 public class marekTesting {
@@ -13,9 +19,6 @@ public class marekTesting {
 	private static final int N = 10;
 	
 	public static void main(String args[]){
-		
-		//temp. wymiar macierzy
-		int wymMacierzy = 10;
 		
 		float h_A[];
 	    float h_B[];
@@ -45,23 +48,35 @@ public class marekTesting {
 	    }
 
 	    /* Allocate device memory for the matrices */
+	    //alokacja miejsca na gpu
 	    JCublas.cublasAlloc(n2, Sizeof.FLOAT, d_A);
 	    JCublas.cublasAlloc(n2, Sizeof.FLOAT, d_B);
 	    JCublas.cublasAlloc(n2, Sizeof.FLOAT, d_C);
 
 	    /* Initialize the device matrices with the host matrices */
+	    //kopiowanie lokalnych wektorow do pamieci gpu
 	    JCublas.cublasSetVector(n2, Sizeof.FLOAT, Pointer.to(h_A), 1, d_A, 1);
 	    JCublas.cublasSetVector(n2, Sizeof.FLOAT, Pointer.to(h_B), 1, d_B, 1);
 	    JCublas.cublasSetVector(n2, Sizeof.FLOAT, Pointer.to(h_C), 1, d_C, 1);
 
+	    /**
+	     * 
+	     * 
+	     * SGEEM - to operacja mnozenia macierzy pojedynczej precyzji
+	     * 
+	     * http://www.jcuda.org/jcuda/jcublas/doc/jcuda/jcublas/JCublas.html
+	     * 
+	     * C = alpha * op(A) * op(B) + beta * C, where op(X) is one of op(X) = X or op(X) = transpose(X)
+	     */
+	    
 	    /* Performs operation using JCublas */
 	    JCublas.cublasSgemm('n', 'n', N, N, N, alpha, d_A, N, d_B, N, beta, d_C, N);
 
 	    /* Read the result back */
+	    //zapisanie z gpu do lokalnej pamieci
 	    JCublas.cublasGetVector(n2, Sizeof.FLOAT, d_C, 1, Pointer.to(h_C), 1);
 
 	    /* Memory clean up */
-
 	    JCublas.cublasFree(d_A);
 	    JCublas.cublasFree(d_B);
 	    JCublas.cublasFree(d_C);
