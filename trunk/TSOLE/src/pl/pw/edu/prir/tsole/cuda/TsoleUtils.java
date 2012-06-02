5dc65b2c19a4ae2c444e5a009ec552250647ed3a
marcinjasion@gmail.com <marcinjasion@gmail.com@4c6f1809-d47f-6a53-e29b-218769a40873>
2012-06-02 18:23:37 +0000

    Wyznacznik

--- trunk/TSOLE/src/pl/pw/edu/prir/tsole/cuda/TsoleUtils.java
+++ trunk/TSOLE/src/pl/pw/edu/prir/tsole/cuda/TsoleUtils.java
@@ -377,29 +377,35 @@ public class TsoleUtils {
 		
 		return result;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public static float det(float[][] mat) {
+		float result = 0;
+
+		if (mat.length == 1) {
+			result = mat[0][0];
+			return result;
+		}
+
+		if (mat.length == 2) {
+			result = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+			return result;
+		}
+
+		for (int i = 0; i < mat[0].length; i++) {
+			float temp[][] = new float[mat.length - 1][mat[0].length - 1];
+
+			for (int j = 1; j < mat.length; j++) {
+				System.arraycopy(mat[j], 0, temp[j - 1], 0, i);
+				System.arraycopy(mat[j], i + 1, temp[j - 1], i, mat[0].length - i - 1);
+			}
+
+			result += mat[0][i] * Math.pow(-1, i) * det(temp);
+		}
+
+		return result;
+
+	}
+
 	
 
 }
